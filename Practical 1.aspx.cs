ac6beb276a9373d57e753c16aaee2c84f9547c23
Yogita Satish Thorat <95094214+iyogitathorat@users.noreply.github.com>
2022-01-12 00:04:30 -0800

    Add files via upload

---
+++ Practical 1.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Yogita__Thorat
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ArrayList al = new ArrayList();
+            string str = "yogita satish thorat";
+            int x = 7;
+            DateTime d = DateTime.Parse("23-may-1998");
+            al.Add(str);
+            al.Add(x);
+            al.Add(d);
+
+            foreach (object o in al)
+            {
+                Response.Write(o);
+                Response.Write("<br>");
+
+
+
+
+
+            }
+        }
+
+    }
+}
+   
